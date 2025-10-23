a68a9d281ad9dc390d1596a835e58ec5e42e44fc
FlorinAsiminei <asiminei_florin@yahoo.com>
2025-10-23 17:13:48 +0300

    solved build bugs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,25 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
-  
-  darkMode: "class",
-// tailwind.config.js
-theme: {
-  extend: {
-    colors: {
-      primary: {
-        DEFAULT: '#34d399', // emerald-400
-        hover: '#10b981',   // emerald-500
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#34d399', // emerald-400
+          hover: '#10b981',   // emerald-500
+        },
       },
     },
   },
-},
-
   corePlugins: {
-    preflight: true, // ✅ Optional: keeps Tailwind's base styles
+    preflight: true,
   },
-
   plugins: [],
-};
+}
+
+export default config
